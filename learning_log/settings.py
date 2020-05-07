ed40df12d375bdb400b80f0798ed6b9a79c9ec61
ikram523 <kakam_007@yahoo.com>
2020-05-08 01:22:13 +0500

    Ready for deployment to heroku.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -133,7 +133,7 @@ BOOTSTRAP3 = {
 }
 
 # Heroku settings
-if os.getcwd() == '/app' or cwd[:4] == '/tmp':
+if os.getcwd() == '/app':
     import dj_database_url
     DATABASES = {
         'default': dj_database_url.config(default='postgres://localhost')
